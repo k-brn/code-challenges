5a4f4ab7163dc79b54085f65e39ad37f58506c32
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-02 16:40:41 -0700

    09-30-2022 challenge

--- 8kyu/8-remove-exclamation-marks.js
+++ 8kyu/8-remove-exclamation-marks.js
@@ -6,7 +6,7 @@ https://www.codewars.com/kata/57a0885cbb9944e24c00008e
 
 /* INSTRUCTIONS
 
-Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string. 
 
 */
 
