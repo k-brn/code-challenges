6e9f4f4035aab967d604f320d561c854ccce126f
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-02 16:46:16 -0700

    09-27-2022 challenge

--- 8kyu/8-dna-to-rna-conversion.js
+++ 8kyu/8-dna-to-rna-conversion.js
@@ -8,7 +8,7 @@ https://www.codewars.com/kata/5556282156230d0e5e000089
 
 Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), 
 
-Adenine ('A'), and Thymine ('T').
+Adenine ('A'), and Thymine ('T'). 
 
 Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is 
 
