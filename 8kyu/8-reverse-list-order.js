ed78adf8d9139cc86317cb4f2f3ece2127b60637
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-02 16:22:37 -0700

    10-02-2022 challenge

--- 8kyu/8-reverse-list-order.js
+++ 8kyu/8-reverse-list-order.js
@@ -25,3 +25,4 @@ function reverseList(list) {
 
 console.log(reverseList([1,2,3,4])); // [4,3,2,1]
 console.log(reverseList([3,1,5,4])); // [4,5,1,3]
+console.log(reverseList([2,4,6,8])); // [8,6,4,2]
