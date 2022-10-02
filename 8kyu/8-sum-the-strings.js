2dde55e90915badb35d80095d878c123a371aaac
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-02 16:52:07 -0700

    09-24-2022 challenge

--- 8kyu/8-sum-the-strings.js
+++ 8kyu/8-sum-the-strings.js
@@ -8,7 +8,7 @@ https://www.codewars.com/kata/5966e33c4e686b508700002d
 
 Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
 
-Example: (Input1, Input2 -->Output)
+Example: (Input1, Input2 -->Output) 
 
 "4",  "5" --> "9"
 "34", "5" --> "39"
