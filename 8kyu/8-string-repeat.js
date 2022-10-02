906290f6b4354c44752848d6e6f3cf928690e668
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-02 16:57:50 -0700

    09-21-2022 challenge

--- 8kyu/8-string-repeat.js
+++ 8kyu/8-string-repeat.js
@@ -8,7 +8,7 @@ https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
 
 Write a function that accepts an integer n and a string s as 
 
-parameters, and returns a string of s repeated exactly n times.
+parameters, and returns a string of s repeated exactly n times. 
 
 Examples (input -> output)
 
