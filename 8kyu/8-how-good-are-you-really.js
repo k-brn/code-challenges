ecdf0a8930cc5f8dda376c389b6357823210d653
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-05 10:10:45 -0700

    10-05-2022 challenge

---
+++ 8kyu/8-how-good-are-you-really.js
@@ -0,0 +1,35 @@
+// How good are you really?
+
+/* URL
+https://www.codewars.com/kata/5601409514fc93442500010b
+*/ 
+
+/* INSTRUCTIONS
+
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+*/
+
+// SOLUTION
+
+function betterThanAverage(classPoints, yourPoints) {
+    return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
+}
+     
+// TEST CASES
+
+console.log(betterThanAverage([2, 3], 5)) // true
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)) // true
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)) // false
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50)) // false
+console.log(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21)) // false
