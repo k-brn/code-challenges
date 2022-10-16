530517c6e8375419f5be1c0fd7e60cc56a5409d8
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-16 11:14:22 -0700

    10-16-2022 challenge

---
+++ 8kyu/8-square-sum.js
@@ -0,0 +1,25 @@
+// Square(n) Sum
+
+/* URL
+https://www.codewars.com/kata/515e271a311df0350d00000f
+*/ 
+
+/* INSTRUCTIONS
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+*/
+
+// SOLUTION
+
+function squareSum(numbers){
+    return numbers.reduce((sum, num) => sum + (num * num), 0)
+}
+     
+// TEST CASES
+
+console.log(squareSum([1,2])) // 5
+console.log(squareSum([0, 3, 4, 5])) // 50
+console.log(squareSum([])) // 0
