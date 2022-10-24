ba823dc3525a85c43bd6a5b3eabed900e227a268
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-23 21:08:43 -0700

    10-23-2022 challenge

---
+++ 8kyu/8-who-ate-the-cookie.js
@@ -0,0 +1,28 @@
+// Who ate the cookie?
+
+/* URL
+https://www.codewars.com/kata/55a996e0e8520afab9000055
+*/ 
+
+/* INSTRUCTIONS
+
+For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+Note: Make sure you return the correct message with correct spaces and punctuation.
+
+*/
+
+// SOLUTION
+
+/*const cookie = (x) => typeof x === 'string' ? 'Who ate the last cookie? It was Zach!' : typeof x === 'number' ? 'Who ate the last cookie? It was Monica!' : 'Who ate the last cookie? It was the dog!'*/
+
+const cookie = x => `Who ate the last cookie? It was ${{string: 'Zach', number: 'Monica'}[typeof x] || 'the dog'}!`
+
+// TEST CASES
+
+console.log(cookie("Ryan")) // "Who ate the last cookie? It was Zach!"
+console.log(cookie(26)) // "Who ate the last cookie? It was Monica!"
+console.log(cookie(2.3)) // "Who ate the last cookie? It was Monica!"
+console.log(cookie(true)) // "Who ate the last cookie? It was the dog!"
