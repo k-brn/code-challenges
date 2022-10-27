8a1e3c5b3a6daf3d88aef8c2b0ed1868daf8b073
k-brn <108495284+k-brn@users.noreply.github.com>
2022-10-27 14:45:19 -0700

    10-27-2022 challenge

---
+++ 8kyu/8-removing-elements.js
@@ -0,0 +1,30 @@
+// Removing Elements
+
+/* URL
+https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
+*/ 
+
+/* INSTRUCTIONS
+
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+
+*/
+
+// SOLUTION
+
+function removeEveryOther(arr){
+    return arr.filter((element,index) => index % 2 === 0)
+}
+     
+// TEST CASES
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])) // ['Hello', 'Hello Again']
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // [1, 3, 5, 7, 9]
+console.log(removeEveryOther([[1, 2]])) // [[1, 2]]
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}])) // [['Goodbye']]
