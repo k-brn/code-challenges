5f267a97e88607a0e2e3c6c2a01b06c127f7ac7a
k-brn <108495284+k-brn@users.noreply.github.com>
2022-12-24 22:06:11 -0800

    12-24-2022 challenge

---
+++ 8kyu/8-bartender-drinks.js
@@ -0,0 +1,56 @@
+// Bartender, drinks!
+
+/* URL
+https://www.codewars.com/kata/568dc014440f03b13900001d
+*/ 
+
+/* INSTRUCTIONS
+
+Complete the function that receives as input a string, and produces outputs according to the following table:
+
+Input	                Output
+"Jabroni"	            "Patron Tequila"
+"School Counselor"	    "Anything with Alcohol"
+"Programmer"	        "Hipster Craft Beer"
+"Bike Gang Member"	    "Moonshine"
+"Politician"	        "Your tax dollars"
+"Rapper"	            "Cristal"
+anything else	        "Beer"
+
+Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+*/
+
+// SOLUTION
+
+function getDrinkByProfession(param){
+    switch (param.toLowerCase()) {
+      case 'jabroni': 
+        return 'Patron Tequila';
+      case 'school counselor': 
+        return 'Anything with Alcohol';
+      case 'programmer': 
+        return 'Hipster Craft Beer';
+      case 'bike gang member': 
+        return 'Moonshine';
+      case 'politician': 
+        return 'Your tax dollars';
+      case 'rapper': 
+        return 'Cristal';
+      default: 
+        return 'Beer';
+    }
+}
+
+// TEST CASES
+
+console.log(getDrinkByProfession("jabrOni")); // "Patron Tequila"
+console.log(getDrinkByProfession("scHOOl counselor")); // "Anything with Alcohol"
+console.log(getDrinkByProfession("prOgramMer")); // "Hipster Craft Beer"
+console.log(getDrinkByProfession("bike ganG member")); // "Moonshine"
+console.log(getDrinkByProfession("poLiTiCian")); // "Your tax dollars"
+console.log(getDrinkByProfession("rapper")); // "Cristal"
+console.log(getDrinkByProfession("pundit")); // "Beer"
+console.log(getDrinkByProfession("Pug")); // "Beer"
