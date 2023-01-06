ea8a942b17371a0954563b86b9b800279256320f
k-brn <108495284+k-brn@users.noreply.github.com>
2023-01-05 21:56:58 -0800

    01-05-2023 challenge

---
+++ 8kyu/8-do-you-speak-english.js
@@ -0,0 +1,29 @@
+// Do you speak "English"?
+
+/* URL
+https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058
+*/ 
+
+/* INSTRUCTIONS
+
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+
+*/
+
+// SOLUTION
+
+function spEng(sentence){
+    // convert string to lowercase, and check to see if it includes 'english'.
+    return sentence.toLowerCase().includes('english');
+}
+
+// TEST CASES
+
+console.log(spEng("english")); // true
+console.log(spEng("egnlish")); // false
