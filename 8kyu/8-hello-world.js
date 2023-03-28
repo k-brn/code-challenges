1cafa609c87dd6ba3d8e2a53e2378ad1fa4249f7
k-brn <108495284+k-brn@users.noreply.github.com>
2023-03-28 09:24:37 -0700

    03-28-2023 challenge

---
+++ 8kyu/8-hello-world.js
@@ -0,0 +1,19 @@
+// hello world
+
+/* URL
+https://www.codewars.com/kata/523b4ff7adca849afe000035
+*/ 
+
+/* INSTRUCTIONS
+
+Make a simple function called greet that returns the most-famous "hello world!".
+
+*/
+
+// SOLUTION
+
+const greet = () => 'hello world!';
+
+// TEST CASES
+
+console.log(greet()); // 'hello world!'
