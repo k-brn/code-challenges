508c09834628e39c5e4fc2affc8d3775a7d5a79b
k-brn <108495284+k-brn@users.noreply.github.com>
2023-05-29 16:48:25 -0700

    05-29-2023 challenge

---
+++ 8kyu/8-remove-duplicates-from-list.js
@@ -0,0 +1,26 @@
+// Remove duplicates from list
+
+/* URL
+https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118
+*/ 
+
+/* INSTRUCTIONS
+
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+*/
+
+// SOLUTION
+
+function distinct(a) {
+    
+    return a.filter((item, index) => a.indexOf(item) === index);
+}
+
+// TEST CASES
+
+console.log(distinct([1])); // [1]
+console.log(distinct([1,2])); // [1,2]
+console.log(distinct([1,1,2])); // [1,2]
