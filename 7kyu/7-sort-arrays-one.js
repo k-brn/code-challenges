fe1509db6f3bc5c2ef4876a55e3d31c2b08fb2f1
k-brn <108495284+k-brn@users.noreply.github.com>
2023-06-18 14:39:23 -0700

    06-18-2023 challenge

---
+++ 7kyu/7-sort-arrays-one.js
@@ -0,0 +1,23 @@
+// Sort arrays - 1
+
+/* URL
+https://www.codewars.com/kata/51f41b98e8f176e70d0002a8
+*/
+
+/* INSTRUCTIONS
+
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+*/
+
+// SOLUTION
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function (names) {
+  return names.sort();
+};
+
+// TEST CASES
+
+console.log(sortme(["one", "two", "three"])); // ["one", "three", "two"]
