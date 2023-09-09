bc61e93c9a0b8952aef527593ca33e09d1a1dc82
k-brn <108495284+k-brn@users.noreply.github.com>
2023-09-08 22:23:11 -0700

    09-08-2023 challenge

---
+++ 8kyu/8-remove-first-and-last-character-part-two.js
@@ -0,0 +1,33 @@
+// Remove First and Last Character Part Two
+
+/* URL
+https://www.codewars.com/kata/570597e258b58f6edc00230d
+*/
+
+/* INSTRUCTIONS
+
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+*/
+
+// SOLUTION
+
+function array(string) {
+  //if (string === "" || string.split(",").length < 3) return null;
+
+  return string.split(",").slice(1, -1).join(" ") || null;
+}
+
+// TEST CASES
+
+console.log(array("")); // null
+console.log(array("1")); // null
+console.log(array("A1,B2")); // null
+console.log(array("1,2,3")); // '2'
+console.log(array("1,2,3,4")); // '2 3'
+console.log(array("A1,B2,C3,D4,E5")); // 'B2 C3 D4'
+console.log(array("A,1,23,456,78,9,Z")); // '1 23 456 78 9'
