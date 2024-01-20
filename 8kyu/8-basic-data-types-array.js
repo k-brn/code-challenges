cf625e9017e01af481716bb5e34985d7622e6691
k-brn <108495284+k-brn@users.noreply.github.com>
2024-01-19 21:05:10 -0800

    01-19-2024 challenge

---
+++ 8kyu/8-basic-data-types-array.js
@@ -0,0 +1,45 @@
+// Basic data types Array
+
+/* URL
+https://www.codewars.com/kata/571effabb625ed9b0600107a
+*/
+
+/* INSTRUCTIONS
+
+I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+
+    1. getLength(arr)    should return length of arr
+    2. getFirst(arr)     should return the first element of arr
+    3. getLast(arr)      should return the last element of arr
+    4. pushElement(arr)  should push an element to arr, and then return arr
+    5. popElement(arr)   should pop an element from arr, and then return arr
+
+*/
+
+// SOLUTION
+
+function getLength(arr) {
+  return arr.length;
+}
+function getFirst(arr) {
+  return arr[0];
+}
+function getLast(arr) {
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  arr.push(1);
+  return arr;
+}
+function popElement(arr) {
+  arr.pop();
+  return arr;
+}
+
+// TEST CASES
+
+console.log(getLength([1, 2, 3])); // 3
+console.log(getFirst([1, 2, 3])); // 1
+console.log(getLast([1, 2, 3])); // 3
+console.log(pushElement([1, 2, 3]).length); // 4
+console.log(popElement([1, 2, 3]).length); // 2
