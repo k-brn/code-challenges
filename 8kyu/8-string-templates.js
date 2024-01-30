2a95bb857f85762d460d752930be6002aa77d816
k-brn <108495284+k-brn@users.noreply.github.com>
2024-01-29 20:59:03 -0800

    01-29-2024 challenge

---
+++ 8kyu/8-string-templates.js
@@ -0,0 +1,23 @@
+// String Templates
+
+/* URL
+https://www.codewars.com/kata/55c90cad4b0fe31a7200001f
+*/
+
+/* INSTRUCTIONS
+
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+*/
+
+// SOLUTION
+
+function buildString(...template) {
+  return `I like ${template.join(", ")}!`;
+}
+
+// TEST CASES
+
+console.log(buildString("Cheese", "Milk", "Chocolate")); // 'I like Cheese, Milk, Chocolate!'
+console.log(buildString("Cheese", "Milk")); // 'I like Cheese, Milk!', 'Return the correct String'
+console.log(buildString("Chocolate")); // 'I like Chocolate!'
