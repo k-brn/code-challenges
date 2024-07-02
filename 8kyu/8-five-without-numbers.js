4f6023f01c6b99a71bdab78a1003edb5d8f1a55e
k-brn <108495284+k-brn@users.noreply.github.com>
2024-07-01 23:18:17 -0700

    coding challenge 07/01/2024

---
+++ 8kyu/8-five-without-numbers.js
@@ -0,0 +1,24 @@
+// 5 without numbers !!
+
+/* URL
+https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+*/
+
+/* INSTRUCTIONS
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+*/
+
+// SOLUTION
+
+function unusualFive() {
+  // return the length of the string "COCKY"
+  return "COCKY".length;
+}
+
+// TEST CASES
+
+console.log(unusualFive()); // 5
